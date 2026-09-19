6ccc7c90f59ab860eac8a0ca6e61cb26a4c7401a
jjk1q <flyingsnowman4@gmail.com>
2026-09-20 00:02:08 +0200

    first commit

---
+++ first/program.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+
+void swap(int *a, int *b);
+
+int main(){
+	int a = 1;
+	int b = 2;
+
+	printf("a: %d, b: %d\n", a, b);
+	swap(&a,&b);
+	printf("a: %d, b: %d\n", a, b);
+	
+	
+}	
+
+
+void swap(int *a, int *b){
+	int c = *a;
+	*a = *b;
+	*b = c;
+	
+}
+
+
+
+
