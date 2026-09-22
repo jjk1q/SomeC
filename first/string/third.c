b96f49ed860c7289362e2e60fa0cb433b77bfa3a
jjk1q <flyingsnowman4@gmail.com>
2026-09-22 12:45:29 +0200

    add third string

---
+++ first/string/third.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void turnAround(char *str);
+int main() {
+
+	char str[100];
+	printf("Text: ");
+	scanf("%s", str);
+	turnAround(str);
+	printf("%s", str);
+
+	
+}
+
+void turnAround(char *str){
+
+	int length = strlen(str);
+	for(int i = 0; i < length / 2; i++){
+		char temp = str[i];
+		str[i] = str[length - i - 1];
+		str[length - i - 1] = temp;
+	}
+}
